5541760466dbc906868a25c9e9cba11be2769189
xuanhung1509 <vuxuanhung15092002@gmail.com>
2022-04-05 15:12:01 +0700

    construct alert context & create alert component

--- src/App.js
+++ src/App.js
@@ -4,26 +4,31 @@ import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Alert from './components/layout/Alert';
 import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className='flex flex-col justify-between h-screen'>
-          <Navbar title='GitHub Finder' />
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar title='GitHub Finder' />
 
-          <main className='container mx-auto px-3 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </main>
+            <main className='container mx-auto px-3 pb-12'>
+              <Alert />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
 
-          <Footer />
-        </div>
-      </Router>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
